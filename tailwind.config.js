48078ffe38d23fe9b4828ddfee76e919931ea2fd
Freddie Heath <fheath090401@gmail.com>
2024-04-30 12:41:54 +0100

    added color configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/**/*.{html,js}"],
   theme: {
+    colors: {
+      ytBlack: "#000000d9",
+      ytBlackTwo: "#111",
+      black: "#000000",
+      white: "#ffffff",
+      ytGrey: "#CCC",
+      ytGreyTwo: "#AAA",
+      ytGreyThree: "#555",
+    },
+
     extend: {},
   },
   plugins: [],
-}
-
+};
